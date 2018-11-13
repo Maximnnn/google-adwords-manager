3ecfde15defbc19275d0880761589a0711b4967b
Maksims Usakovs <m.usakovs@dynatech.lv>
2018-11-13 11:31:00 +0200

    remove negative kw from adGroup

--- controllers/AdGroup.php
+++ controllers/AdGroup.php
@@ -8,7 +8,13 @@ use App\Http\Response;
 use App\Validation\Rules;
 use Google\AdsApi\AdWords\AdWordsServices;
 use Google\AdsApi\AdWords\AdWordsSession;
+use Google\AdsApi\AdWords\v201809\cm\AdGroupCriterionService;
 use Google\AdsApi\AdWords\v201809\cm\AdGroupService;
+use Google\AdsApi\AdWords\v201809\cm\CampaignCriterionOperation;
+use Google\AdsApi\AdWords\v201809\cm\CriterionType;
+use Google\AdsApi\AdWords\v201809\cm\CriterionUse;
+use Google\AdsApi\AdWords\v201809\cm\KeywordMatchType;
+use Google\AdsApi\AdWords\v201809\cm\Operator;
 use Google\AdsApi\AdWords\v201809\cm\OrderBy;
 use Google\AdsApi\AdWords\v201809\cm\Paging;
 use Google\AdsApi\AdWords\v201809\cm\Predicate;
@@ -64,5 +70,172 @@ class AdGroup extends BaseController {
         } while ($selector->getPaging()->getStartIndex() < $totalNumEntries);
 
         return $result;
+    }
+
+    public function removeNegativeKeywords(Request $request) {
+
+        $session = $this->getSession($this->getOAuth2());
+        $services = new AdWordsServices();
+
+        $request->validate(Rules::create([
+            'adgroup_id' => 'required|notempty|int',
+            'keywords' => 'required|notempty'
+        ]), 'post');
+
+        $adgroupId = $request->post('adgroup_id', 'int');
+        $keywords = $request->post('keywords');
+
+        $phrase_kws = [];
+        $exact_kws = [];
+        $broad_kws = [];
+        $criteria_to_delete = [];
+        $operations = [];
+
+        foreach ($keywords as $keyword) {
+            if ($keyword[0] == '[') {
+                $exact_kws[] = str_replace(['[', ']'], '', $keyword);
+            } else if ($keyword[0] == '"') {
+                $phrase_kws[] = str_replace('"', '', $keyword);
+            } else {
+                $broad_kws[] = $keyword;
+            }
+        }
+
+        /**@var $service AdGroupCriterionService*/
+        $service = $services->get($session, AdGroupCriterionService::class);
+
+        //EXACT
+        if (!empty($exact_kws)) {
+            $selector = new Selector();
+            $selector->setFields(['KeywordText']);
+            $selector->setPredicates(
+                [
+                    new Predicate('AdGroupId', PredicateOperator::IN, [$adgroupId]),
+                    new Predicate(
+                        'CriteriaType',
+                        PredicateOperator::IN,
+                        [CriterionType::KEYWORD]
+                    ),
+                    new Predicate(
+                        'KeywordMatchType',
+                        PredicateOperator::IN,
+                        [KeywordMatchType::EXACT]
+                    ),
+                    new Predicate(
+                        'KeywordText',
+                        PredicateOperator::IN,
+                        $exact_kws
+                    ),
+                    new Predicate(
+                        'CriterionUse',
+                        PredicateOperator::IN,
+                        [CriterionUse::NEGATIVE]
+                    )
+                ]
+            );
+
+            $page = $service->get($selector);
+            if ($page) {
+                $entries = $page->getEntries();
+                if ($entries) foreach ($entries as $criterion) {
+                    $criteria_to_delete[] = $criterion;
+                };
+            }
+        }
+
+        //PHRASE
+        if (!empty($phrase_kws)) {
+            $selector = new Selector();
+            $selector->setFields(['KeywordText']);
+            $selector->setPredicates(
+                [
+                    new Predicate('AdGroupId', PredicateOperator::IN, [$adgroupId]),
+                    new Predicate(
+                        'CriteriaType',
+                        PredicateOperator::IN,
+                        [CriterionType::KEYWORD]
+                    ),
+                    new Predicate(
+                        'KeywordMatchType',
+                        PredicateOperator::IN,
+                        [KeywordMatchType::PHRASE]
+                    ),
+                    new Predicate(
+                        'KeywordText',
+                        PredicateOperator::IN,
+                        $phrase_kws
+                    ),
+                    new Predicate(
+                        'CriterionUse',
+                        PredicateOperator::IN,
+                        [CriterionUse::NEGATIVE]
+                    )
+                ]
+            );
+
+            $page = $service->get($selector);
+
+            if ($page) {
+                $entries = $page->getEntries();
+                if ($entries) foreach ($entries as $criterion) {
+                    $criteria_to_delete[] = $criterion;
+                };
+            }
+        }
+
+        //BROAD
+        if (!empty($broad_kws)) {
+            $selector = new Selector();
+            $selector->setFields(['KeywordText']);
+            $selector->setPredicates(
+                [
+                    new Predicate('AdGroupId', PredicateOperator::IN, [$adgroupId]),
+                    new Predicate(
+                        'CriteriaType',
+                        PredicateOperator::IN,
+                        [CriterionType::KEYWORD]
+                    ),
+                    new Predicate(
+                        'KeywordMatchType',
+                        PredicateOperator::IN,
+                        [KeywordMatchType::BROAD]
+                    ),
+                    new Predicate(
+                        'KeywordText',
+                        PredicateOperator::IN,
+                        $broad_kws
+                    ),
+                    new Predicate(
+                        'CriterionUse',
+                        PredicateOperator::IN,
+                        [CriterionUse::NEGATIVE]
+                    )
+                ]
+            );
+
+            $page = $service->get($selector);
+            if ($page) {
+                $entries = $page->getEntries();
+                if ($entries) foreach ($entries as $criterion) {
+                    $criteria_to_delete[] = $criterion;
+                };
+            }
+        }
+
+        foreach ($criteria_to_delete as $criterion) {
+            $operation = new CampaignCriterionOperation();
+            $operation->setOperand($criterion);
+            $operation->setOperator(Operator::REMOVE);
+
+            $operations[] = $operation;
+        }
+
+        if (!empty($operations)) {
+            $service->mutate($operations);
+            $result = ['deleted' => count($operations)];
+        } else
+            $result = ['deleted' => 0];
+
+        return Response::getInstance($result)->type('json');
     }
 }
